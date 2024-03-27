7daf40038f890961dff76f967bbef1b5a24d725f
javierdv00 <dv.javier@gmail.com>
2024-03-27 15:56:26 +0100

    some defs

--- file_1.py
+++ file_1.py
@@ -8,18 +8,104 @@ def printc(string, color="white"):
     # string first, then color optional - default white
     print(cs(string,color))
 
-def room1():
-    print("Your enter in room1.")
-    print("Text...........")
-    option_1 = "..........option 1........."
-    option_2 = "..........option 2........."
-    option_3 = "..........option 3........."
+def take_stairs():
+    print("""As you move towards the stairs, you notice a dark shadow standing at the top of the stairs. You can't make out its exact outline, but you can feel it staring at you and it makes you feel uncomfortable!""")
+    print("Do you want to continue up the stairs?")
+    option_1 = "Yes"
+    option_2 = "No"
+    #option_3 = "..........option 3........."
+    #printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
+    printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
+    decision = input("CHOOSE: ").lower()
+    while decision not in ["1", "2", "3", option_1.lower(), option_2.lower(), option_3.lower(), "y","n"]:
+        printc("INVALID INPUT, please choose between:.","red")
+        #printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
+        printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
+        decision = input("CHOOSE: ").lower()
+    
+    if decision in ["1","yes","y"]:
+        print('''You continue up the stairs, as you get closer and closer to the shadow, you feel your heart beating faster and faster and the presence of this shadow makes you feel very uncomfortable, but in the blink of an eye the shadow disappears before you reach the top of the stairs.
+Confused and worried, you ask yourself "Was that just my imagination? What could possibly happen to me here?" ''')
+        return "1. floor"
+    elif decision in ["2","no","n"]:
+        print('You turn back into the Main Hall.')
+        return "main hall"
+
+def 1.floor():
+    print("""You are now standing in a corridor where you can see a door to the left, right and in front of each one, which door do you go in?  """)
+    option_1 = "Left"
+    option_2 = "Front"
+    option_3 = "Terminal"
+    #printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
     printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
     decision = input("CHOOSE: ").lower()
     while decision not in ["1", "2", "3", option_1.lower(), option_2.lower(), option_3.lower()]:
         printc("INVALID INPUT, please choose between:.","red")
+        #printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
         printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
         decision = input("CHOOSE: ").lower()
+    
+    if decision in ["1",option_1]:
+        print('''You slowly open the door and hear a soft rustling sound, it sounds like someone is shuffling through papers. Between piles of paper is a very stressed and nervous employee of the Agentur für Arbeit. "Excuse me, please..." you say quietly into the office. The counselor looks at you in horror and under stress he shouts in your direction "All my colleagues are sick, I have to do all the paperwork for them - please go to office 204, above us" and slams the door from the inside.  ''')
+        return "office_204"
+    elif decision in ["2",option_2]:
+        print('A big Potato comes out of the shadows and dragg you into the darkness. You died, what did you expect.')
+        #return ""
+    elif decision in ["3",option_3]:
+        print('.............')
+        return ""
+
+def office_204():
+    print("""  """)
+    print("""  """)
+    option_1 = "......option 1 ......"
+    option_2 = ".....option 2......."
+    #option_3 = "..........option 3........."
+    printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
+    #printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
+    decision = input("CHOOSE: ").lower()
+    while decision not in ["1", "2", "3", option_1.lower(), option_2.lower(), option_3.lower()]:
+        printc("INVALID INPUT, please choose between:.","red")
+        printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
+        #printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
+        decision = input("CHOOSE: ").lower()
+    
+    if decision in ["1",option_1]:
+        print('''  ''')
+        return ""
+    elif decision in ["2",option_2]:
+        print('')
+        return ""
+    elif decision in ["3",option_3]:
+        print('')
+        return ""
+
+def name():
+    print("""  """)
+    print("""  """)
+    option_1 = "......option 1 ......"
+    option_2 = ".....option 2......."
+    #option_3 = "..........option 3........."
+    printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
+    #printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
+    decision = input("CHOOSE: ").lower()
+    while decision not in ["1", "2", "3", option_1.lower(), option_2.lower(), option_3.lower()]:
+        printc("INVALID INPUT, please choose between:.","red")
+        printc("1- "+option_1+"/n"+"2- "+option_2, "cyan")
+        #printc("1- "+option_1+"/n"+"2- "+option_2+"/n"+"3- "+option_3, "cyan")
+        decision = input("CHOOSE: ").lower()
+    
+    if decision in ["1",option_1]:
+        print('''  ''')
+        return ""
+    elif decision in ["2",option_2]:
+        print('')
+        return ""
+    elif decision in ["3",option_3]:
+        print('')
+        return ""
+
+
 
 #---------END OF FUNCTIONS-----------------------------------------------
 
