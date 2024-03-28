4c7873fee2f39a9bbba76028ba1935f988d3f0ca
Morgane Ortic <morgane.ortic@proton.me>
2024-03-28 09:26:18 +0100

    added more lines

--- german_mystery.py
+++ german_mystery.py
@@ -6,15 +6,17 @@ def printc(string, color="white"):
     # string first, then color optional - default white
     print(cs(string,color))
 
-intro = ''' The employment agency asks you to submit the same document for the 10th time - so you go to the office in a rage to resolve the matter once and for all.
+# Introduction text
+intro = '''The employment agency asks you to submit the same document for the 10th time - so you go to the office in a rage to resolve the matter once and for all.
 
 When you arrive at the office, you find yourself standing in an empty hall with not a soul around and wonder where your point of contact might be. You think about where you are most likely to find someone who can help you with your matter. '''
-
+# short break after intro
 def main():
     printc(intro, "blue")
     time.sleep(3)
     start()
-    
+
+# Start of the actual game
 def start():
     print("\nWhat do you do?")
     while True:
@@ -61,53 +63,14 @@ def stairs():
             print("Invalid choice. Please try again.")
             time.sleep(0.5)
 
-# If you bang on the door
-def hall():
-    print("\nYou bang loudly on the door. You hear some metallic clinking as a grumpy voice shouts from the other side:")
-    time.sleep(1)
-    printc("\n'What the fuck is going on? Shut the fuck up or I'll make you!'", "red")
-    time.sleep(0.5)
-    print("\nWhat do you do?")
-    while True:
-        print("\n1. Bang louder")
-        print("2. 'Oh, sorry! I won't do it again'")
-        print("3. 'Please, can you help me?? My precious golden ring rolled to a corner and I can't find it. :('")
-        choice = input("\nYour choice: ")
-        if choice == '1':
-            bang_door()
-        elif choice == '2':
-            start()
-        elif choice == '3':
-            lost_ring()
-        else:
-            print("Invalid choice. Please try again.")
-            time.sleep(0.5)
-
-# If you lure the guard into believing you have a golden ring to seize
-def lost_ring():
-    print("\nA short silence.")
-    time.sleep(1)
-    print("You hear a key rotating in the lock. The voice says:")
-    printc("\n'Alright, but no funny stuff! I'm armed and I'm a tough one!'", "red")
-    time.sleep(0.5)
-    print("\nWhat do you do?")
-    while True:
-        print("\n1. Nothing")
-        print("2. Hide behind the door")
-        print("2. Hide against the wall on the other side of the door")
-        choice = input("\nYour choice: ")
-
-        """
-        if choice == '1':
-            bang_door()
-        elif choice == '2':
-            start()
-        elif choice == '3':
-            lost_ring()
-        else:
-            print("Invalid choice. Please try again.")
-            time.sleep(0.5)
-            """
+#game over if you wait too long
+def gameover_hall():
+    print("\nYou turn back into the Main Hall.")
+    time.sleep(2)
+    print("\n[you are waiting in the hall until you met a member of staff]")
+    time.sleep(5)
+    print("\nYou've been waiting unnecessarily for 3 hours for something to happen. The employment agency closes for the day.")
+    printc("GAME OVER", "red")
 
 if __name__ == "__main__":
     main()
